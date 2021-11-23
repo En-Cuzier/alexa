3374746f813ec725695479fc2c855ecf0181535e
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-11-23 22:57:13 +0530

    Update Animelist.js

--- plugins/Animelist.js
+++ plugins/Animelist.js
@@ -1,6 +1,6 @@
 const Neotro = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const ANIME = "\n\n  *👩‍🦰Amazone Alexa Anime List*\n\n╔═════════▷\n╠🌼: .loli\n╠🌼: .wifu\n╠🌼: .neko\n╠🌼: .ava\n╠🌼: .kemo\n╠🌼: .awoo\n╠🌼: .shinobu\n╠🌼: .megumin\n╚═════════▷"
+const ANIME = "\n\n  *   *⦁◊═⦁QUEEN 👸 ALEXA⦁═◊⦁*\n\n╔═════════▷\n╠🏵: .loli\n╠🏵: .wifu\n╠🏵: .neko\n╠🏵: .ava\n╠🏵: .kemo\n╠🏵: .awoo\n╠🏵: .shinobu\n╠🏵: .megumin\n╚═════════▷"
 const fs = require('fs');
 const Ln = "Anime List"
 const axios = require('axios');
