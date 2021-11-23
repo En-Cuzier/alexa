039cb8fd79098b9f1afca94cdf8c1c7b15ac5dd8
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-11-23 11:39:44 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -5,13 +5,13 @@ NEOTROX - TEENUHX
 */
 
 function successfullMessage(msg) {
-    return "👩‍🦰 *AMAZONE ALEXA*:  ```" + msg + "```"
+    return "🧝‍♀️ *QUEEN ALEXA*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🚀 *AMAZONE ALEXA*:  ```" + msg + "```"
+    return "🛡 *QUEEN ALEXA*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "🤖 *AMAZONE ALEXA*:  ```" + msg + "```"
+    return "🤖 *QUEEN ALEXA*:  ```" + msg + "```"
 }
 
 
