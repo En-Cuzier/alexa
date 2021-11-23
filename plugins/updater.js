1bed10abf5e5246dcb2b6091803db0e4dbba21e1
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-11-23 12:54:56 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -30,7 +30,7 @@ Asena.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (a
         var degisiklikler = Lang.NEW_UPDATE;
         commits['all'].map(
             (commit) => {
-                degisiklikler += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' ◁◁' + commit.author_name + '▷▷\n';
+                degisiklikler += '🛡 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' ◁◁' + commit.author_name + '▷▷\n';
             }
         );
         
