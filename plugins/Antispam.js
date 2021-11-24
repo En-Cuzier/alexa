b1742393f46b3baea67be441e296de5f69070d31
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-11-24 11:27:44 +0530

    Create Antispam.js

--- plugins/Antispam.js
+++ plugins/Antispam.js
@@ -8,14 +8,14 @@ const Neotro = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
 const Config = require('../config');
-const code = ('🚀A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n👾 ' + Config.BOT_NAME + '   \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\🚀AntiSpam By ' + Config.BOT_NAME + ' n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n👾 ' + Config.BOT_NAME + '  👾Anti Spam System👾 \n*Powered By 👩‍🦰Queen Amazone Alexa*')
+const code = ('🛡A\nN\nT\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n👾 ' + Config.BOT_NAME + '   \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\🛡AntiSpam By ' + Config.BOT_NAME + ' 🛡n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n👾 ' + Config.BOT_NAME + '  👾Anti Spam System👾 \n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ǫᴜᴇᴇɴ ᴀʟᴇxᴀ*')
 const FINAL = "THIS IS AN ANTISAPM (anti lag),\nThis code prevents your phone from getting stuck due to malicious messages"
-const TMUTE = "🚀Trying to close temporary,\n🚀Attempting to temporarily close the group"
+const TMUTE = "🛡Trying to close temporary,\n🚀Attempting to temporarily close the group"
 const MUT = ".mute 1d"
-const SCXR = "🧚‍♂️Running script...."
+const SCXR = "🧝‍♀️Running script...."
 Neotro.addCommand({pattern: 'antispm', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
 
-  var msg = await message.reply('👩‍🦰Preforming....');
+  var msg = await message.reply('*🛡Preforming....*');
 
   await message.client.sendMessage(
     message.jid,TMUTE, MessageType.text);
