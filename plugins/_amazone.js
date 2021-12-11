3453186c80ad153d68dece7069d868e0ba183469
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-12-11 14:32:24 +0530

    Update _amazone.js

--- plugins/_amazone.js
+++ plugins/_amazone.js
@@ -64,7 +64,7 @@ if (Config.WORKTYPE == 'private') {
             );
             
             await message.client.sendMessage(message.jid, lusifarv, MessageType.audio, {mimetype: 'audio/mp4', ptt:true}, {quoted: message.data})
-            await message.client.sendMessage(message.jid, '◄◉◉◉◉◉◉◉◯◉◯◉◯◉◉◉◉◉◉◉◉►\n\n◁' + Config.BOT_NAME + '▷\n_Private Bot_\n\n' + Config.MENU_INFO + '\n\n◄◉◉◉◉◉◉◯◉◯◉◯◉◉◉◉◉◉◉◉►\nᴾᵒʷᵉʳᵉᵈ ᴮʸ ᴬᴹᴬᶻᴼᴺᴱ🛸ᴬᴸᴱˣᴬ\n\n' + CMD_HELP, MessageType.text,{quoted: message.data}); 
+            await message.client.sendMessage(message.jid, '*◄━━━━━━━⦁═⦁❶⦁═⦁━━━━━━━►*\n\n❲' + Config.BOT_NAME + '❳\n*◄━━━━━━━⦁⦁═✪⦁═⦁━━━━━━━►*\n\n' + Config.MENU_INFO + '\n\n𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 🧝‍♀️ Qᵘᵉᵉⁿ ᵃˡᵉˣᵃ\n\n' + CMD_HELP, MessageType.text,{quoted: message.data}); 
        }
        else {
             var CMD_HELP = '';
